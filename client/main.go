49e1f907546e347c4d9d0f9f984492d40df5847a
Seth Ek <sge3141@gmail.com>
2025-09-17 15:16:44 -0500

    Server-Client skeleton setup

--- client/main.go
+++ client/main.go
@@ -1,9 +1,28 @@
 package main
 
 import (
-	"fmt"
+	"log"
+	"net"
+)
+
+const (
+	SOCKET       = "127.0.0.1:10740"
+	SIZE_OF_BUFF = 1024
 )
 
 func main() {
-	fmt.Println("Hello from the client")
+	// Connect to the socket via tcp
+	conn, err := net.Dial("tcp", SOCKET)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Write a basic message
+	message := []byte("Hello from the client!")
+	_, err = conn.Write(message)
+	if err != nil {
+		log.Println(err)
+	}
+
+	conn.Close()
 }
--- server/main.go
+++ server/main.go
@@ -13,25 +13,42 @@ import (
 )
 
 const (
+	IP = "127.0.0.1"
 	PORT = ":10740"
+	SIZE_OF_BUFF = 1024
 )
 
 func handleConnection(conn net.Conn) {
+	defer conn.Close() // Close connection upon function exit
 
+	// Read in data sent by the connection
+	buffer := make([]byte, SIZE_OF_BUFF)		// Creates a buffer slice
+	bytesRead, err := conn.Read(buffer)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	for i := 0; i < bytesRead; i++ {
+		fmt.Print(string(buffer[i]))
+	}
+	fmt.Println("")
 }
 
 func main() {
+	// Start the server
 	ln, err := net.Listen("tcp", PORT)
 	if err != nil {
 		log.Fatal(err)
 		return
 	}
-
+	defer ln.Close() // Close upon exiting main
 	fmt.Println("Server is listening on port ", PORT)
+
+	// Listen for and handle connections
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
-			log.Fatal(err)
+			log.Println(err)
 			continue
 		}
 		go handleConnection(conn)
